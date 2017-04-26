ee95419c6dff5e70853683978fcbe401a5a2a7ac
Dmitry <medavedik@gmail.com>
2017-04-26 23:31:51 +0300

    Task: news feed #2. (#9)
    * Task: news feed #2.
    Добавил модель Post  с полями:
    title:string
    description:text
    body:text
    
    * Task: news feed #2.
    Добавил контроллер Post, actions: index, show.
    
    * Task: news feed #2.
    Добавил контроллер Post, actions: index, show.
    Fix: routes
    
    * Task: news feed #2.
    Добавил контроллер Post, actions: index, show.
    Fix: routes.
    
    * Task: news feed #2.
    Добавил контроллер Post, actions: index, show.
    Fix: routes.
    
    * Task: news feed #2.
    Добавил контроллер Post, actions: index, show.
    Fix: routes.(Style guide)
    
    * Task: news feed #2.
    Добавил контроллер Post, actions: index, show.
    Добавил стандартные вьюхи.
    
    * Удалил не нужные билдеры.

---
+++ db/migrate/20170426111235_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :description
+      t.text :body
+
+      t.timestamps null: false
+    end
+  end
+end
