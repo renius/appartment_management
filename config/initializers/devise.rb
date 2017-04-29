8d4184db07c19b271b4802bb1ffc3dcf525abbaa
DmitryEvgenevich <medavedik@gmail.com>
2017-04-30 02:28:53 +0300

    Task: devise authentication #1. Удалил возможность переподтверждения email'а.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,6 @@ Devise.setup do |config|
   config.strip_whitespace_keys = [:email]
   config.skip_session_storage = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 11
-  config.reconfirmable = true
   config.expire_all_remember_me_on_sign_out = true
   config.password_length = 6..128
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
